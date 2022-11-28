f4b53d6704dd001812b095b956b58be665944540
diogoheck <diogo.heck.dev@gmail.com>
2022-11-28 16:27:24 -0300

    Versao_2

--- clean_unico_CTB.py
+++ clean_unico_CTB.py
@@ -4,19 +4,38 @@ import shutil
 
 lista_proibidos = ['Trabalhista']
 pasta_sincronizacao = ['.sync']
+LOCAL_GRAVAR_LOGS = 'R:\Compartilhado\Ti\Limpeza_Unico_log\log.txt'
+
+
+def salvar_logs(conteudo):
+    with open(LOCAL_GRAVAR_LOGS, 'a') as log:
+        print(conteudo, file=log)
+
+
+def remover_arquivo(arquivo):
+    try:
+        os.remove(arquivo)
+        salvar_logs(f'{arquivo} removido com sucesso')
+    except Exception as E:
+        salvar_logs(E)
+        
 
 
 def limpeza_unico_pastas():
 
     folder_list = []
+    
     os.chdir('U:\\')
     cwd = os.getcwd()
+
+    if os.path.exists(LOCAL_GRAVAR_LOGS):
+        os.remove(LOCAL_GRAVAR_LOGS)
 
     for i in os.listdir(cwd):
         if os.path.isdir(i) and i not in lista_proibidos and i not in pasta_sincronizacao:
             folder_list.append(i)
         elif i not in lista_proibidos and i not in pasta_sincronizacao and '.py' not in i:
-            os.remove(i)
+            remover_arquivo(i)
             pass
 
     for folder_raiz in folder_list:
@@ -24,7 +43,7 @@ def limpeza_unico_pastas():
             path2 = cwd + '\\' + folder_raiz
             os.chdir(path2)
             sub_folder_list = [i if os.path.isdir(
-                i) else os.remove(i) for i in os.listdir(path2)]
+                i) else remover_arquivo(i) for i in os.listdir(path2)]
         
         for subfolder in sub_folder_list:
             if subfolder:
@@ -34,15 +53,15 @@ def limpeza_unico_pastas():
                     if folder2:
                         try:
                             if os.path.isfile(folder2):
-                                os.remove(folder2)
+                                remover_arquivo(folder2)
                             else:
                                 shutil.rmtree(folder2)
+                        except PermissionError:
+                            pass
                         except:
                             pass
-    print('')
-    print('*' * 50)
-    print('limpeza efetuada com sucesso!!')
-    print('*' * 50)
-    print('')
+    
+    salvar_logs('limpeza efetuada com sucesso!!')
+    
 
 limpeza_unico_pastas()
